b4f7dd3b8b0fb7f876ab342df6660fe174200aa0
Leonardo Loureiro <83887814+LeoLoureiro-code@users.noreply.github.com>
2024-08-17 19:26:36 -0700

    Add files via upload

---
+++ Hubs/ChatHub.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace TalksterAPI.Hubs
+{
+    public class ChatHub :Hub
+    {
+        public async Task JoinGroup(string groupName, string userName)
+        {
+            await Groups.AddToGroupAsync(Context.ConnectionId, groupName);
+
+            await Clients.Group(groupName).SendAsync("NewUser", $"{userName} enter the chat");
+        }
+
+        public async Task LeaveGroup(string groupName, string userName)
+        {
+            await Groups.RemoveFromGroupAsync(Context.ConnectionId, groupName);
+
+            await Clients.Group(groupName).SendAsync("LeftUser", $"{userName} leave the chat");
+        }
+
+        public async Task SendMessage(NewMessage message)
+        {
+            await Clients.Group(message.GroupName).SendAsync("NewMessage", message);
+
+        }
+
+        public record NewMessage(string userName,  string message, string GroupName); 
+    }
+}
